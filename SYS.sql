5e47bbcaed36dc621838a8ff007445d7869a40c0
fk-Prince <prince.sestoso@gmail.com>
2025-10-22 23:04:09 +0800

    Update SYS.sql

--- SYS.sql
+++ SYS.sql
@@ -18,6 +18,9 @@
 --
 -- Table structure for table `category`
 --
+DROP DATABASE IF EXISTS final_ordering_system;
+CREATE DATABASE final_ordering_system;
+USE final_ordering_system;
 
 DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -2363,3 +2366,4 @@ DELIMITER ;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2025-10-22 23:03:07
+
